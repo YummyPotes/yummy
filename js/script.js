b088291c3fe4bd8b1a1f611d69ea2e6fd6d65084
YummyPotes <91847025+YummyPotes@users.noreply.github.com>
2021-10-03 16:04:22 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 
+var pegaProd = document.querySelector("#imgprod1");
+  pegaProd.src="img/coxinhademorango.jpg";
+
+var pegaProd2 = document.querySelector("#imgprod2");
+  pegaProd2.src="img/minivulcao.jpg";
+
+var pegaProd3 = document.querySelector("#imgprod3");
+  pegaProd3.src="img/palhaitaliana.jpg";
+
+var pegaProd4 = document.querySelector("#imgprod4");
+  pegaProd4.src="img/brigadeirogourmet.jpg";
+
+var pegaProd5 = document.querySelector("#imgprod5");
+  pegaProd5.src="img/caseirinho.jpg";
+
+var pegaProd6 = document.querySelector("#imgprod6");
+  pegaProd6.src="img/barrarecheada.jpg";
+
+
+
